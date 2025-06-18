6688336cb531c2250c4bb90fa354098220093ce6
itweed <ian.tweedie@capgemini.com>
2025-06-18 13:00:52 +0100

    Remove sensitive auth.json file and add launchWithProfile.js for browser testing; update package-lock.json with license information.

--- playwright.config.ts
+++ playwright.config.ts
@@ -29,8 +29,9 @@ export default defineConfig({
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    //trace: 'on-first-retry',
     trace: 'on-first-retry',
-    
+
     /* Enable video recording */
     video: 'on',
 
